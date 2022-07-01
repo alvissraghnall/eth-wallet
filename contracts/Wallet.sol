88c16c1bbef0c09f197619c49e6a369e5a4c6aaa
Alviss Raghnall <isiomastewart@gmail.com>
2022-06-30 22:11:21 -0700

    arrdee - come & go

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -1,9 +1,17 @@
-pragma solidity >=0.6.0 <0.8.13;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
 
 /// @title ETH Wallet Contract
 /// @author Alviss Raghnall
 /// @notice Stores ETH balance, and processes transactions.
 /// @dev Explain to a developer any extra details
 contract Wallet {
+
+    mapping(address => uint256) public addressToAmountReceived;
+
+    function balance () external view returns (uint) {
+        return address(this).balance;
+    }
     
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-
+const router = require("./router");
 const app = express();
 const PORT = process.env.PORT || 8798;
 
 app.use(express.static(__dirname + '../public'));
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); 
 
 app.use(router);
 
